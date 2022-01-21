70bd37d48d20aa1410e997a47d724fcc16e38d44
philkwek-PC <64081785+philkwek@users.noreply.github.com>
2022-01-21 16:47:03 +0800

    Added firebase

---
+++ Assets/Scripts/FirebaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Database;
+
+public class FirebaseTest : MonoBehaviour
+{
+    DatabaseReference databaseRef;
+
+    private void Awake()
+    {
+        databaseRef = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        databaseRef.Child("test").SetValueAsync("test!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
