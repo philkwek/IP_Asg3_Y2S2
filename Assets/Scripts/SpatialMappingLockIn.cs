a03c03998e27262f61f9bc066620f1d4c6844027
pitdapat <chew.patrick1@gmail.com>
2022-01-24 14:36:22 +0800

    Getting start with surface magnatism

---
+++ Assets/Scripts/SpatialMappingLockIn.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * This script file is used in the Introduction to Spatial Mapping tutorial.
+ */
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using UnityEngine;
+
+/// <summary>
+/// Component that contains helpful functions for working with Spatial Mapping
+/// </summary>
+public class SpatialMappingLockIn : MonoBehaviour
+{
+    /// <summary>
+    /// Function to lock in the spatial mapping mesh
+    /// </summary>
+    public void LockInSpatialMapping()
+    {
+        // Introduction to Spatial Mapping: Exercise 6.5
+        /************************************************************/
+        // Stop the spatial mapping scanning using "CoreServices.SpatialAwarenessSystem"
+        
+
+        // Introduction to Spatial Mapping: Exercise 7.3.a
+        /************************************************************/
+        // Get the IMixedRealityDataProviderAccess object from the Spatial Awareness System 
+        
+        // Introduction to Spatial Mapping: Exercise 7.3.b
+        /************************************************************/
+        // Get a list of Spatial Awareness Mesh Observers from the IMixedRealityDataProviderAccess object
+        
+        // Introduction to Spatial Mapping: Exercise 7.3.c
+        /************************************************************/
+        // Go through every Spatial Awareness Mesh Observer and set its display option to None or Occluded
+        
+
+
+
+
+    }
+}
