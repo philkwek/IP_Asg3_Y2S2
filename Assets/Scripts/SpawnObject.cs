ba16882c167d5cd4a29875c749b6d3897698daa5
pitdapat <chew.patrick1@gmail.com>
2022-01-27 14:35:44 +0800

    Added some models with texture and starting with buttons

---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpawnObject : MonoBehaviour
+{
+    public void Spawn(GameObject objectToSpawn)
+    {
+        Vector3 buttonT = this.transform.position;
+        var spawnedOject = Instantiate(objectToSpawn, this.transform);
+    }
+}
