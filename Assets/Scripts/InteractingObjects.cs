3c41e9b8bedf476206e64dc6b56c40c4445670e0
pitdapat <chew.patrick1@gmail.com>
2022-01-27 16:29:03 +0800

    Implemented gaze tough code and tested some functionality
    Added sceneunderstanding sdk too but might not use depending on understanding

---
+++ Assets/Scripts/InteractingObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class InteractingObjects : MonoBehaviour, IMixedRealityFocusHandler
+{
+    GameManager gmScript;
+
+    private void Start()
+    {
+        gmScript = FindObjectOfType<GameManager>();
+    }
+
+    public void OnFocusEnter(FocusEventData eventData)
+    {
+        Debug.Log($"{this.gameObject.name} has entered gaze");
+        if (gmScript != null)
+        {
+            gmScript.objectInGaze = this.gameObject;
+        }
+    }
+
+    public void OnFocusExit(FocusEventData eventData)
+    {
+        Debug.Log($"{this.gameObject.name} has left gaze!!!");
+    }
+}
