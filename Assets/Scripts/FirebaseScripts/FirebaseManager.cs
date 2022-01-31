b60a42fa596d0439c04690a97f35905051a52791
philkwek-PC <64081785+philkwek@users.noreply.github.com>
2022-01-31 16:51:10 +0800

    Setup firebase code to get info from realtimedb

--- Assets/Scripts/FirebaseScripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseScripts/FirebaseManager.cs
@@ -4,22 +4,21 @@ using Models;
 using Proyecto26;
 using System.Collections.Generic;
 using UnityEngine.Networking;
+using TMPro;
+using FullSerializer;
 
 
-public class FirebaseTest : MonoBehaviour
+public class FirebaseManager : MonoBehaviour
 {
-    //test variables
-    public string username = "test";
-    public int userScore;
 
-    public string email = "test@test.com";
-    public string password = "testing123";
+    private string restLink = "https://ip-asg3-y2s2-default-rtdb.firebaseio.com";
+    private string AuthKey = "AIzaSyB4iLqQhetQzvpCJIhczEZrRlF3daOsXJI"; //api key for firebase project 
 
     //account data
-    public string AuthKey = "AIzaSyB4iLqQhetQzvpCJIhczEZrRlF3daOsXJI"; //api key for firebase project 
-
     public string idToken;
-    public static string localId; //this is the unique account authId
+    public static string localId; //this is the unique account authID
+
+    public static fsSerializer serializer = new fsSerializer();
 
     private void Awake()
     {
@@ -29,15 +28,30 @@ public class FirebaseTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PostToDatabase();
-        CreateAccount(email, username, password);
+        GetDatabaseIds();
     }
 
     //example function for posting data to database
     private void PostToDatabase()
     {
-        string link = "https://ip-asg3-y2s2-default-rtdb.firebaseio.com/" + username + ".json";
-        RestClient.Put(link, new User("test", 5));
+        //string link = restLink + username + ".json";
+        //RestClient.Put(link, new User("test", 5));
+    }
+
+    private void GetDatabaseIds() 
+    {
+        Debug.Log("Getting from Database");
+        RestClient.Get(restLink + "/users.json").Then(response =>
+        {
+            fsData userData = fsJsonParser.Parse(response.Text); //converts text into json
+            Debug.Log(userData);
+            Dictionary<string, User> users = null; 
+            serializer.TryDeserialize(userData, ref users); //tries to translate json into the declared user array
+            foreach (var user in users.Values)
+            {
+                Debug.Log(user.username);
+            }
+        });
     }
 
     //example function for creating account with firebase
@@ -49,6 +63,11 @@ public class FirebaseTest : MonoBehaviour
             {
                 idToken = response.idToken;
                 localId = response.localId;
+
+                //Create account in the RealtimeDb from inputs
+                string link = restLink + "users/" + localId + ".json";
+                User newUser = new User(email, username, localId);
+                RestClient.Put(link, newUser);
             }
        ).Catch(error =>
        {
---
+++ Assets/Scripts/FirebaseScripts/MenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    public FirebaseManager firebaseManager;
+
+    //Sign up Menu Inputs
+    public TextMeshProUGUI createUsernameInput;
+    public TextMeshProUGUI createEmailInput;
+    public TextMeshProUGUI createPasswordInput;
+
+    //Sign in Menu Inputs
+    public TextMeshProUGUI emailInput;
+    public TextMeshProUGUI passwordInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateAccount()
+    {
+        firebaseManager.CreateAccount(createEmailInput.text, createUsernameInput.text, createPasswordInput.text);
+    }
+
+    public void SignInAccount()
+    {
+        firebaseManager.SignInAccount(emailInput.text, passwordInput.text);
+    }
+}
