52bfe80b04c59d662e9e3ec146d837d0b3a20f4a
pitdapat <chew.patrick1@gmail.com>
2022-02-08 21:24:52 +0800

    Added script function for toggling script
    I want to cry

--- Assets/Scripts/ModelLibrary.cs
+++ Assets/Scripts/ModelLibrary.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.Unity.VisualStudio.Editor;
+using Microsoft.MixedReality.Toolkit.UI.BoundsControl;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+using Microsoft.MixedReality.Toolkit.Input;
 
 public class ModelLibrary : MonoBehaviour
 {
@@ -19,5 +24,29 @@ public class ModelLibrary : MonoBehaviour
         menu.SetActive(!menu.activeInHierarchy);
     }
 
+    /// <summary>
+    /// Used to trigger script activation on button pressed
+    /// </summary>
+    /// <param name="modelLibObject">This is to refence the object that holds these script</param>
+    public void TriggerScriptForMainLibrary(GameObject modelLibObject)
+    {
+        //Ensures you are able to get reference
+        if(modelLibObject != null)
+        {
+            //Finds script reference
+            RadialView radialScript = modelLibObject.GetComponent<RadialView>();
 
+            BoundsControl boundscript = modelLibObject.GetComponent<BoundsControl>();
+            NearInteractionGrabbable grabScript = modelLibObject.GetComponent<NearInteractionGrabbable>();
+            ObjectManipulator manipulateScript = modelLibObject.GetComponent<ObjectManipulator>();
+
+            //If the radial script is enable then disable the ability to grab and scale. 
+            boundscript.enabled = !radialScript.enabled;
+            grabScript.enabled = !radialScript.enabled;
+            manipulateScript.enabled = !radialScript.enabled;
+        } else
+        {
+            Debug.Log("<color=red>FIX THE REFERENCE TO LIBRARY</color>");
+        }
+    }
 }
