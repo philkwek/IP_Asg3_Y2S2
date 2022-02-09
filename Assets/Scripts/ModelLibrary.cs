482b8f57f0c0e7f413d6c82c4682fc0740eacfec
pitdapat <chew.patrick1@gmail.com>
2022-02-09 17:12:45 +0800

    Exported

---
+++ Assets/Scripts/ModelLibrary.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class ModelLibrary : MonoBehaviour
+{
+    public GameObject[] toDisableMenu;
+
+    private void Start()
+    {
+        for (int i = 0; i < toDisableMenu.Length; i++)
+        {
+            toDisableMenu[i].SetActive(false);
+        }
+    }
+
+    public void ToggleMenu(GameObject menu)
+    {
+        menu.SetActive(!menu.activeInHierarchy);
+    }
+
+    /// <summary>
+    /// Used to trigger script activation on button pressed
+    /// </summary>
+    /// <param name="modelLibObject">This is to refence the object that holds these script</param>
+    public void TriggerScriptForMainLibrary(GameObject modelLibObject)
+    {
+        //Ensures you are able to get reference
+        if (modelLibObject != null)
+        {
+            //Finds script reference
+            RadialView radialScript = modelLibObject.GetComponent<RadialView>();
+
+            BoundingBox boundscript = modelLibObject.GetComponent<BoundingBox>();
+            NearInteractionGrabbable grabScript = modelLibObject.GetComponent<NearInteractionGrabbable>();
+            ObjectManipulator manipulateScript = modelLibObject.GetComponent<ObjectManipulator>();
+
+            //If the radial script is enable then disable the ability to grab and scale. 
+            boundscript.enabled = !radialScript.enabled;
+            grabScript.enabled = !radialScript.enabled;
+            manipulateScript.enabled = !radialScript.enabled;
+        }
+        else
+        {
+            Debug.Log("<color=red>FIX THE REFERENCE TO LIBRARY</color>");
+        }
+    }
+}
