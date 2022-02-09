f8fa6d3224285c57c88627c7e633f28800c84a70
philkwek <philkwek03@gmail.com>
2022-02-09 16:34:19 +0800

    Revert "Updated functions for converting images into base64 data"
    This reverts commit 75f8b2b447f5dcb9f630fefb9c5e6bf20bded15f.

--- Assets/Scripts/FirebaseScripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseScripts/FirebaseManager.cs
@@ -25,6 +25,7 @@ public class FirebaseManager : MonoBehaviour
        
     //references for img upload test
     public Texture2D imgTex;
+    byte[] bytes;
 
     private void Awake()
     {
@@ -69,12 +70,6 @@ public class FirebaseManager : MonoBehaviour
         });
     }
 
-    public void ConvertImage(Texture2D img){ //Function for converting texture2d img to base64 string
-        byte[] bytes = img.EncodeToPng();   //create a var called bytes with byte[]
-        string imageData = ConvertToBase64String(bytes);    //convert the img data to a base 64 string
-        return imageData;
-    }
-
     public void SaveProject()
     {
         string creator = localId;
@@ -82,14 +77,15 @@ public class FirebaseManager : MonoBehaviour
         int companyId = userCompanyId;
 
         //convert img to bytes before converting to a base64 for realtimedb storage
-        string imageData = ConvertImage(imgTex);
+        bytes = imgTex.EncodeToPNG();
+        string str = Convert.ToBase64String(bytes);
 
         //below are test values
         string[] furnitureUsed = { "chair", "table" }; //get furniture
         string houseType = "HDB"; //get housetype
         string nameOfLayout = "Test Layout"; //get name of layout
         int noOfBedrooms = 5; //get no of bedrooms
-        string[] pictures = { imageData }; //get no of pictures taken (max 3)
+        string[] pictures = { str }; //get no of pictures taken (max 3)
 
         Project newProject = new Project(companyId, creator, dateCreated, furnitureUsed, houseType, nameOfLayout, noOfBedrooms, pictures);
         string link = restLink + "/projects/.json?auth=" + idToken;
